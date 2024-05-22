44baf4f1355d75ec65bcb658f826338b14874d01
Bruno Neves <bruno.neves@exaud.com>
2024-05-22 18:47:39 +0100

    folder img changes

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,15 +1,16 @@
+"use strict";
 function searchFunction() {
-    var input, filter, sections, i, txtValue;
-    input = document.querySelector('input[name="search"]');
-    filter = input.value.toUpperCase();
-    sections = document.getElementsByTagName("section");
+  var input, filter, sections, i, txtValue;
+  input = document.querySelector('input[name="search"]');
+  filter = input.value.toUpperCase();
+  sections = document.getElementsByTagName("section");
 
-    for (i = 0; i < sections.length; i++) {
-        txtValue = sections[i].textContent || sections[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            sections[i].style.display = "";
-        } else {
-            sections[i].style.display = "none";
-        }
+  for (i = 0; i < sections.length; i++) {
+    txtValue = sections[i].textContent || sections[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      sections[i].style.display = "";
+    } else {
+      sections[i].style.display = "none";
     }
+  }
 }
--- js/showImages.js
+++ js/showImages.js
@@ -1,13 +1,15 @@
+"use strict";
+
 function abrirImagem(elemento) {
-    var imagemModal = document.getElementById("imagem-modal");
-    imagemModal.src = elemento.src;
-    document.getElementById("modal").style.display = "flex";
+  var imagemModal = document.getElementById("imagem-modal");
+  imagemModal.src = elemento.src;
+  document.getElementById("modal").style.display = "flex";
 }
 
 function fecharImagem() {
-    document.getElementById("modal").style.display = "none";
+  document.getElementById("modal").style.display = "none";
 }
 
 function fecharImagem() {
-document.getElementById("modal").style.display = "none";
+  document.getElementById("modal").style.display = "none";
 }
